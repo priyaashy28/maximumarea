1fb0055bd59de53b05132d333f474dc0f0a34790
priyaashy28 <58980496+priyaashy28@users.noreply.github.com>
2024-01-08 23:45:36 +0000

    updated the App js

--- maximumarea/src/App.js
+++ maximumarea/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const findMaximumAreaTravelled = (arrHeights, n) => {
+    var area = 0;
+    for (let i = 0; i < n; i++) {
+      var left_index;
+      var right_index;
+
+      let j;
+      for (j = i; j >= 0; j--) {
+        if (arrHeights[j] < arrHeights[i]) {
+          left_index = j;
+          break;
+        }
+      }
+      left_index = j;
+      let k;
+      for (k = i; k < n; k++) {
+        if (arrHeights[k] < arrHeights[i]) {
+          right_index = k;
+          break;
+        }
+      }
+      right_index = k;
+
+      area = Math.max(area, arrHeights[i] * (right_index - left_index - 1));
+    }
+    return area;
+  };
+
+  const arrHeights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{findMaximumAreaTravelled(arrHeights, arrHeights.length)}</h3>
     </div>
   );
 }
